8f93fe7b3346b04597293f3fea9ce58602bbd4a9
kusabashira <kusabashira227@gmail.com>
2015-10-20 22:05:04 +0900

    Implement CSVScanner as rough

--- csvp.go
+++ csvp.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
+	"io"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 var FIELD = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
@@ -19,4 +22,54 @@ func parseIndexesList(list string) ([]int, error) {
 		nums[i] = num
 	}
 	return nums, nil
+}
+
+type CSVScanner struct {
+	indexes []int
+	text    string
+	err     error
+	reader  *csv.Reader
+}
+
+func NewCSVScanner(indexes []int, r io.Reader) *CSVScanner {
+	return &CSVScanner{
+		indexes: indexes,
+		reader:  csv.NewReader(r),
+	}
+}
+
+func (c *CSVScanner) Err() error {
+	if c.err == io.EOF {
+		return nil
+	}
+	return c.err
+}
+
+func (c *CSVScanner) Bytes() []byte {
+	return []byte(c.text)
+}
+
+func (c *CSVScanner) Text() string {
+	return c.text
+}
+
+func (c *CSVScanner) Scan() bool {
+	if c.err != nil {
+		return false
+	}
+
+	recode, err := c.reader.Read()
+	if err != nil {
+		c.err = err
+		c.text = ""
+		return false
+	}
+
+	var values []string
+	for _, index := range c.indexes {
+		values = append(values, recode[index])
+	}
+	c.text = strings.Join(values, "\t")
+
+	return true
 }
