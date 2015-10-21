20d8e9dba7d4e0c69f07a4f12e0d90563029f3a7
kusabashira <kusabashira227@gmail.com>
2015-10-21 19:30:32 +0900

    Implement Indexes as rough

--- selector.go
+++ selector.go
@@ -5,3 +5,29 @@ type Selector interface {
 	ParseHeader(header []string) error
 	Select(recode []string) ([]string, error)
 }
+
+type Indexes struct {
+	indexes []int
+}
+
+func NewIndexes(indexes []int) *Indexes {
+	return &Indexes{
+		indexes: indexes,
+	}
+}
+
+func (i *Indexes) RequireHeader() bool {
+	return false
+}
+
+func (i *Indexes) ParseHeader(header []string) error {
+	return nil
+}
+
+func (i *Indexes) Select(recode []string) ([]string, error) {
+	var values []string
+	for _, index := range i.indexes {
+		values = append(values, recode[index])
+	}
+	return values, nil
+}
