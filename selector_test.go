94f2b3d26a483787edd944781bea01e6f3d863a0
kusabashira <kusabashira227@gmail.com>
2015-10-26 00:13:26 +0900

    Add test for Indexes

--- selector_test.go
+++ selector_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -35,6 +36,108 @@ func TestSelectAll(t *testing.T) {
 		if !reflect.DeepEqual(actual, expect) {
 			t.Errorf("All.Select(%q) = %q, want %q",
 				test.src, actual, expect)
+		}
+	}
+}
+
+var selectIndexesTests = []struct {
+	description string
+	list        string
+	src         [][]string
+	dst         [][]string
+}{
+	{
+		description: "no input",
+		list:        "1",
+		src:         [][]string{},
+		dst:         [][]string{},
+	},
+	{
+		description: "only one index",
+		list:        "1",
+		src: [][]string{
+			{"aaa", "bbb", "ccc"},
+			{"ddd", "eee", "fff"},
+		},
+		dst: [][]string{
+			{"aaa"},
+			{"ddd"},
+		},
+	},
+	{
+		description: "index out of bounds",
+		list:        "4",
+		src: [][]string{
+			{"aaa", "bbb", "ccc"},
+			{"ddd", "eee", "fff"},
+		},
+		dst: [][]string{
+			{""},
+			{""},
+		},
+	},
+	{
+		description: "multiple indexes",
+		list:        "3,1",
+		src: [][]string{
+			{"aaa", "bbb", "ccc"},
+			{"ddd", "eee", "fff"},
+		},
+		dst: [][]string{
+			{"ccc", "aaa"},
+			{"fff", "ddd"},
+		},
+	},
+	{
+		description: "duplicated indexes",
+		list:        "2,2,2",
+		src: [][]string{
+			{"aaa", "bbb", "ccc"},
+			{"ddd", "eee", "fff"},
+		},
+		dst: [][]string{
+			{"bbb", "bbb", "bbb"},
+			{"eee", "eee", "eee"},
+		},
+	},
+	{
+		description: "battery",
+		list:        "8,8,2,1,1,4",
+		src: [][]string{
+			{"a", "bb", "ccc", "dddd", "eeeee"},
+			{"f", "gg", "hhh", "iiii", "jjjjj"},
+			{"j", "kk", "lll", "mmmm", "nnnnn"},
+		},
+		dst: [][]string{
+			{"", "", "bb", "a", "a", "dddd"},
+			{"", "", "gg", "f", "f", "iiii"},
+			{"", "", "kk", "j", "j", "mmmm"},
+		},
+	},
+}
+
+func TestSelectIndexes(t *testing.T) {
+	for _, test := range selectIndexesTests {
+		i, err := NewIndexes(test.list)
+		if err != nil {
+			t.Errorf("NewIndexes(%q) returns %q, want nil",
+				test.list, err)
+		}
+		self := fmt.Sprintf("{list=%q, description=%q}",
+			test.list, test.description)
+
+		expect := test.dst
+		actual := make([][]string, len(test.src))
+		for j, line := range test.src {
+			actual[j], err = i.Select(line)
+			if err != nil {
+				t.Errorf("%s: Select(%q) returns %q, want nil",
+					self, line, err)
+			}
+		}
+		if !reflect.DeepEqual(actual, expect) {
+			t.Errorf("%s: %q: got %q, want %q",
+				self, test.src, actual, expect)
 		}
 	}
 }
