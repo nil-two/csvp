2562f32b7be2475a2061928faa7f22ef16a5a80e
kusabashira <kusabashira227@gmail.com>
2016-02-27 21:09:20 +0900

    Upgrade to ignore headers in all input-files if --headers specified

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	name    = "csvp"
-	version = "0.9.0"
+	version = "0.10.0"
 
 	flagset         = pflag.NewFlagSet(name, pflag.ContinueOnError)
 	indexesList     = flagset.StringP("indexes", "i", "", "")
